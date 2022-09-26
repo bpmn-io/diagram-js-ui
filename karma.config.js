0e758d0dc6f2f1760c930768f8f25639acdad965
Beatriz Mendes <bea.smendes98@gmail.com>
2022-09-26 16:09:00 +0200

    chore: initial setup

---
+++ karma.config.js
@@ -0,0 +1,129 @@
+const path = require('path');
+const { DefinePlugin } = require('webpack');
+
+const babelConfig = require('./.babelrc');
+
+const basePath = '.';
+
+// configures browsers to run test against
+// any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE', 'PhantomJS' ]
+const browsers = (process.env.TEST_BROWSERS || 'ChromeHeadless').split(',');
+
+const singleStart = process.env.SINGLE_START;
+
+const coverage = process.env.COVERAGE;
+
+const absoluteBasePath = path.resolve(path.join(__dirname, basePath));
+
+// use puppeteer provided Chrome for testing
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+const suite = coverage ? 'test/coverageBundle.js' : 'test/testBundle.js';
+
+module.exports = function(karma) {
+
+  const config = {
+
+    basePath,
+
+    frameworks: [
+      'webpack',
+      'mocha',
+      'sinon-chai'
+    ],
+
+    files: [
+      suite
+    ],
+
+    preprocessors: {
+      [ suite ]: [ 'webpack', 'env' ]
+    },
+
+    reporters: [ 'progress' ].concat(coverage ? 'coverage' : []),
+
+    coverageReporter: {
+      reporters: [
+        { type: 'lcov', subdir: '.' },
+      ]
+    },
+
+    browsers,
+
+    singleRun: true,
+    autoWatch: false,
+
+    webpack: {
+      mode: 'development',
+      module: {
+        rules: [
+          {
+            test: /\.(css)$/,
+            use: 'raw-loader'
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                ...babelConfig,
+                plugins: babelConfig.plugins.concat(coverage ? (
+                  [
+                    [ 'istanbul', {
+                      include: [
+                        'src/**'
+                      ]
+                    } ]
+                  ]
+                ) : [])
+              }
+            }
+          },
+          {
+            test: /\.svg$/,
+            use: [ 'react-svg-loader' ]
+          },
+          {
+            test: /\.png$/,
+            use: [ 'file-loader' ]
+          }
+        ]
+      },
+      plugins: [
+        new DefinePlugin({
+
+          // @barmac: process.env has to be defined to make @testing-library/preact work
+          'process.env': {}
+        })
+      ],
+      resolve: {
+        mainFields: [
+          'browser',
+          'module',
+          'main'
+        ],
+        alias: {
+          'preact': '/preact',
+          'react': '/preact/compat',
+          'react-dom': '/preact/compat'
+        },
+        modules: [
+          'node_modules',
+          absoluteBasePath
+        ],
+        fallback: {
+          'crypto': false
+        }
+      },
+      devtool: 'eval-source-map'
+    }
+  };
+
+  if (singleStart) {
+    config.browsers = [].concat(config.browsers, 'Debug');
+    config.envPreprocessor = [].concat(config.envPreprocessor || [], 'SINGLE_START');
+  }
+
+  karma.set(config);
+};
---
+++ rollup.config.js
@@ -0,0 +1,75 @@
+import path, { parse as parsePath, relative as relativePath } from 'path';
+import { replaceInFile } from 'replace-in-file';
+
+import babel from '@rollup/plugin-babel';
+import copy from 'rollup-plugin-copy';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-import-css';
+
+import pkg from './package.json';
+import babelConfig from './.babelrc.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        sourcemap: true,
+        format: 'commonjs',
+        file: pkg.main
+      },
+      {
+        sourcemap: true,
+        format: 'esm',
+        file: pkg.module
+      }
+    ],
+    external: [
+      'classnames',
+      'min-dash',
+      'min-dom',
+
+      // exclude local preact copy to share it with extensions
+      /\.\/preact/
+    ],
+    plugins: [
+      copy({
+
+        // hook name provided to make sure next plugin has files to replace
+        hook: 'buildStart',
+        targets: [
+          { src: 'node_modules/preact', dest: '.' }
+        ]
+      }),
+      rewirePreactSubpackages(),
+      babel({ ...babelConfig, babelHelpers: 'bundled' }),
+      json(),
+      resolve(),
+      css()
+    ]
+  }
+];
+
+/**
+ * Monkey-patch preact subpackages to import from the local package via relative path.
+ */
+function rewirePreactSubpackages() {
+  return {
+    async buildEnd() {
+      await replaceInFile({
+        files: './preact/**/*.{js,mjs,js.map}',
+        from: [ /(import\s*['"])preact([/'"])/g, /(from\s*['"])preact([/'"])/g, /(require\(['"])preact([/'"])/g ],
+        to: function(...args) {
+          const importGroup = args[1],
+                afterImport = args[2],
+                filePath = args.pop();
+
+          const { dir } = parsePath(filePath);
+          const posixPathToPreact = relativePath(dir, './preact').split(path.sep).join(path.posix.sep);
+          return `${importGroup}${posixPathToPreact}${afterImport}`;
+        }
+      });
+    }
+  };
+}
