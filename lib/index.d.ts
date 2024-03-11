4bc5e993ef63b7c65201713a55190e843976453e
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-03-11 16:24:18 +0100

    fix: correct type exports

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,17 @@
 export { html } from 'htm/preact';
 
 export * from 'preact';
-export * from 'preact/hooks';
+export {
+  useState,
+  useRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useDebugValue,
+  useErrorBoundary,
+  useLayoutEffect,
+  useId,
+  useMemo,
+  useReducer,
+  useImperativeHandle
+} from 'preact/hooks';
